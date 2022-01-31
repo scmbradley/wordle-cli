d477c2e84ba12b1b1c3a11a43e49901cc9b0f654
Seamus Bradley <github@scmb.xyz>
2022-01-31 17:06:38 +0000

    feature: Draw graph of first experiment

--- solver_experiment.py
+++ solver_experiment.py
@@ -3,6 +3,8 @@ from wordle import Wordle
 from wordle_solver import WordleSolver
 from scipy.stats import describe
 
+import matplotlib.pyplot as plt
+
 
 def print_result_stats(solver_output):
     values = [x[0] for x in solver_output]
@@ -16,8 +18,7 @@ w = Wordle(base_word_list="multi.txt", verbose=False)
 
 s = WordleSolver(wordle=w)
 
-ew = s.exclusive_words(no_doubles=False)
-ew += ["MUDDY"]
+ew = s.exclusive_words(permit_doubles=True)
 
 NUM_SOLVES = 8192
 
@@ -40,3 +41,19 @@ print("Do three words first:")
 three_words = s.multisolver(NUM_SOLVES, firsts=ew)
 
 print_result_stats(three_words)
+
+# alt_res = s.multisolver(NUM_SOLVES, firsts=alt)
+
+# print_result_stats(alt_res)
+
+baseline_values = [x[0] for x in baseline]
+two_values = [x[0] for x in two_words]
+three_values = [x[0] for x in three_words]
+plt.close("all")
+plt.hist(
+    [baseline_values, two_values, three_values],
+    bins,
+    label=["baseline", "two words", "three words"],
+)
+plt.legend(loc="upper right")
+plt.savefig("experiment1.png")
